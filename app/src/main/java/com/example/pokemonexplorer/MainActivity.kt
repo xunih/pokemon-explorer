4c9a1e6a3b5fdc1ffafd2f67f853326d5e0cf185
Xuni Huang <xunih@outlook.com>
2022-09-24 23:18:14 +0200

    Add pokemon image

--- app/src/main/java/com/example/pokemonexplorer/MainActivity.kt
+++ app/src/main/java/com/example/pokemonexplorer/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.pokemonexplorer.interfaces.PokemonInterface
 import com.example.pokemonexplorer.presenter.PokemonPresenter
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
     private var number = 1
     private var numberString = "1"
     private var presenter: PokemonPresenter? = null
+    private var img_url = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,12 +22,19 @@ class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
         val textView2 = findViewById<TextView>(R.id.textView2)
         textView2.text = numberString
         presenter?.apiCall(numberString!!)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        img_url =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$numberString.png"
+        Picasso.get().load(img_url).into(imageView)
         val plusBtn = findViewById<Button>(R.id.plusBtn)
         plusBtn.setOnClickListener {
             number += 1;
             numberString = number.toString();
             println(numberString)
             presenter?.apiCall(numberString!!)
+            img_url =
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$numberString.png"
+            Picasso.get().load(img_url).into(imageView)
         }
         val minusBtn = findViewById<Button>(R.id.minusBtn)
         minusBtn.setOnClickListener {
@@ -33,6 +43,9 @@ class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
                 numberString = number.toString();
                 println(numberString)
                 presenter?.apiCall(numberString!!)
+                img_url =
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$numberString.png"
+                Picasso.get().load(img_url).into(imageView)
             }
         }
     }
