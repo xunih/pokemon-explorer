af7a8558ba5100423ae11ec631f1a6e8a4c495db
Xuni Huang <xunih@outlook.com>
2022-09-24 21:35:07 +0200

    Add number editor update

--- app/src/main/java/com/example/pokemonexplorer/MainActivity.kt
+++ app/src/main/java/com/example/pokemonexplorer/MainActivity.kt
@@ -9,26 +9,38 @@ import com.example.pokemonexplorer.interfaces.PokemonInterface
 import com.example.pokemonexplorer.presenter.PokemonPresenter
 
 class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
-    private var number = 0
+    private var number = 1
+    private var numberString = "1"
     private var presenter: PokemonPresenter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         presenter = PokemonPresenter(this)
+        val textView2 = findViewById(R.id.textView2) as TextView
+        textView2.text = numberString
+        presenter?.apiCall(numberString!!)
         val plusBtn = findViewById(R.id.plusBtn) as Button
         plusBtn.setOnClickListener {
-            println("Number is ")
-            number = number + 1;
-            println(number);
-            var num = number.toString();
-            println("Num id is ")
-            println(num)
-            presenter?.apiCall(num!!)
+            number += 1;
+            numberString = number.toString();
+            println(numberString)
+            presenter?.apiCall(numberString!!)
+        }
+        val minusBtn = findViewById(R.id.minusBtn) as Button
+        minusBtn.setOnClickListener {
+            if (number !== 1) {
+                number -= 1;
+                numberString = number.toString();
+                println(numberString)
+                presenter?.apiCall(numberString!!)
+            }
         }
     }
 
     override fun updateViewData() {
         val textView1 = findViewById(R.id.textView1) as TextView
         textView1.text = presenter?.showPokemonName()
+        val textView2 = findViewById(R.id.textView2) as TextView
+        textView2.text = numberString
     }
 }
