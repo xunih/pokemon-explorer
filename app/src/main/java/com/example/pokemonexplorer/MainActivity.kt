272e54f8df1771cd9e24caa245f442b745a6b05f
Xuni Huang <xunih@outlook.com>
2022-09-25 15:58:02 +0200

    Add number editor

--- app/src/main/java/com/example/pokemonexplorer/MainActivity.kt
+++ app/src/main/java/com/example/pokemonexplorer/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.pokemonexplorer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import com.example.pokemonexplorer.interfaces.PokemonInterface
 import com.example.pokemonexplorer.presenter.PokemonPresenter
 import com.squareup.picasso.Picasso
@@ -19,8 +23,8 @@ class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
         setContentView(R.layout.activity_main)
         presenter?.changeMinusBtn(false)
         presenter = PokemonPresenter(this)
-        val textView2 = findViewById<TextView>(R.id.textView2)
-        textView2.text = numberString
+        val textView2 = findViewById<EditText>(R.id.textView2)
+        textView2.setText(numberString)
         presenter?.apiCall(numberString!!)
         val plusBtn = findViewById<Button>(R.id.plusBtn)
         plusBtn.setOnClickListener {
@@ -36,10 +40,41 @@ class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
                 numberString = number.toString();
                 presenter?.apiCall(numberString!!)
             }
-            if (number == 1){
+            if (number == 1) {
                 presenter?.changeMinusBtn(false)
             }
         }
+
+        textView2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val text = p0.toString()
+                if (!text.isNullOrBlank() &&
+                    !text.isEmpty() &&
+                    !text.isNullOrEmpty() &&
+                    text.isDigitsOnly() &&
+                    text.toIntOrNull() !== null
+                ) {
+                    if (text == numberString) {
+                    } else {
+                        numberString = text;
+                        number = text.toInt()
+                        presenter?.apiCall(numberString!!)
+                        if (number !== 1) {
+                            presenter?.changeMinusBtn(true)
+                        } else {
+                            presenter?.changeMinusBtn(false)
+                        }
+                    }
+                }
+
+            }
+        })
     }
 
     override fun updateViewData() {
@@ -51,8 +86,8 @@ class MainActivity : AppCompatActivity(), PokemonInterface.PokemonView {
         val textView1 = findViewById<TextView>(R.id.textView1)
         textView1.text =
             (presenter?.showPokemonDetails()?.second) + " " + (presenter?.showPokemonDetails()?.first)
-        val textView2 = findViewById<TextView>(R.id.textView2)
-        textView2.text = (presenter?.showPokemonDetails()?.second)
+        val textView2 = findViewById<EditText>(R.id.textView2)
+        textView2.setText(presenter?.showPokemonDetails()?.second)
         val imageView = findViewById<ImageView>(R.id.imageView)
         img_url =
             "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + presenter?.showPokemonDetails()?.second + ".png"
